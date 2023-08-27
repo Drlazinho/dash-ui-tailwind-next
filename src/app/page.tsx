aa87704411899f8aeb9a0643165dbcde15e00c3c
drlazinho <lazbonfim@hotmail.com>
2023-08-27 17:07:30 -0300

    Responsividade e Breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='p-8 bg-slate-900 text-slate-100 h-screen'>
-      <h2 className="font-bol text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 ">Hello Tailwind</h2>
+      <h2 className="font-bol text-3xl sm:text-5xl lg:text-6lx  flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500 ">Hello Tailwind</h2>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
       <button className="bg-sky-500 px-4 py-2 rounded-md font-medium enabled:hover:bg-sky-600 disabled:opacity-60 disabled:cursor-not-allowed">Sign in</button>
